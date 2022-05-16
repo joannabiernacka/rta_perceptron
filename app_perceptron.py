6c4fd0cf2734b6247384bee2902e2b3b7ab26274
joannabiernacka <61385986+joannabiernacka@users.noreply.github.com>
2022-05-16 14:33:38 +0200

    Update app_perceptron.py

--- app_perceptron.py
+++ app_perceptron.py
@@ -6,7 +6,6 @@ import pickle
 import redis
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
 with open("perceptron_model_iris.pkl", "rb") as model:    
     iris_model = pickle.load(model)
     
